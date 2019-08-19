a02bf2899163770765c6ba5025d34e25785af38e
xsilent007 <xsilent1337@gmail.com>
2019-08-19 11:44:00 +0200

    Update Brain.cs

--- RuntimeFucker/Brain.cs
+++ RuntimeFucker/Brain.cs
@@ -4,24 +4,19 @@
 
         //Taken from https://gist.github.com/gszauer/f1a2e0beef15a73ac107
         class Brainfuck {
-            string _out;
-            internal static string Start(string bfProgram) {
-                var bf = new Brainfuck(bfProgram);
-                bf.Fuck();
-
-                return bf._out;
-            }
+            internal static string Start(string bfProgram) => new Brainfuck(bfProgram).Fuck();
 
             internal Brainfuck(string program) => _program = program;
 
             string _program;
             int _ptr;
             readonly byte[] _memory = new byte[5096];
-            internal void Fuck() {
+            internal string Fuck() {
+                var builder = new StringBuilder();
                 for (var i = 0; i < _program.Length; i++) {
                     switch (_program[i]) {
                         case '.':
-                            _out += (char)_memory[_ptr];
+                            builder.Append((char)_memory[_ptr]);
                             break;
                         case '<':
                             if (_ptr == 0)
@@ -71,6 +66,8 @@
                             break;
                     }
                 }
+                
+                return builder.ToString();
             }
         }
     }
